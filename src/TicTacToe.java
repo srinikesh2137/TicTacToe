26dff39c364c693050658dfed811f79dd9f313c5
srinikesh2137 <srinikesh1728@gmail.com>
2026-04-28 22:21:06 +0530

    UC4: convertion of slot in tictactoe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
-        int slot = getUserInput();
-        System.out.println("You selected slot: " + slot);
+        int slot = 7;
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
     }
-    public static int getUserInput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a slot number (1-9): ");
-        int slot = sc.nextInt();
-        while (slot < 1 || slot > 9) {
-            System.out.print("Invalid input! Enter number between 1-9: ");
-            slot = sc.nextInt();
-        }
-
-        return slot;
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
