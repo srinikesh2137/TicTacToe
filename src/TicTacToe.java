30d6dc546fad63534b3566b4cad3562c09222e82
srinikesh2137 <srinikesh1728@gmail.com>
2026-04-28 22:02:22 +0530

    UC1: printing tictactoe board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,23 @@
-public class TicTacToe{
+public class TicTacToe {
     public static void main(String[] args){
-
+        char[][] board = new char[3][3];
+        initializeBoard(board);
+        printBoard(board);
+    }
+    public static void initializeBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+    public static void printBoard(char[][] board) {
+        System.out.println("Tic-Tac-Toe Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println(); // Move to row
+        }
     }
 }
